b8ca749e9f6b6912923578822b9650f784b1f0fe
Bobby Johnson <bobby@madnesslabs.net>
2021-01-28 13:40:58 -0600

    fix(): Update dependencies

--- src/components/editor/editor.tsx
+++ src/components/editor/editor.tsx
@@ -65,96 +65,100 @@ export class EnjinEditor implements ComponentInterface {
       },
       placeholder: this.placeholder,
       holder: this.editorEl,
-      tools: {...{
-        paragraph: {
-          class: Paragraph,
-          inlineToolbar: true,
-        },
-        header: {
-          class: Header,
-          inlineToolbar: true,
-        },
-        button: {
-          class: Button,
-        },
-        image: {
-          class: ImageTool,
-          config: {
-            uploader: {
-              uploadByFile: (file) => {
-                return new Promise((resolve, reject) => {
-                  const uploadTask = firebase
-                    .storage()
-                    .ref(`/users/${this.userId}/${file.name}`)
-                    .put(file, {});
-                  uploadTask.on(
-                    "state_changed",
-                    function (snapshot) {
-                      var progress =
-                        (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                      console.log("Upload is " + progress + "% done");
-                      switch (snapshot.state) {
-                        case firebase.storage.TaskState.PAUSED: // or 'paused'
-                          console.log("Upload is paused");
-                          break;
-                        case firebase.storage.TaskState.RUNNING: // or 'running'
-                          console.log("Upload is running");
-                          break;
-                      }
-                    },
-                    function (error) {
-                      reject(error);
-                    },
-                    function () {
-                      uploadTask.snapshot.ref
-                        .getDownloadURL()
-                        .then(function (url) {
-                          resolve({
-                            success: true,
-                            file: {
-                              url,
-                            },
+      tools: {
+        ...{
+          paragraph: {
+            class: Paragraph,
+            inlineToolbar: true,
+          },
+          header: {
+            class: Header,
+            inlineToolbar: true,
+          },
+          button: {
+            class: Button,
+          },
+          image: {
+            class: ImageTool,
+            config: {
+              uploader: {
+                uploadByFile: (file) => {
+                  return new Promise((resolve, reject) => {
+                    const uploadTask = firebase
+                      .storage()
+                      .ref(`/users/${this.userId}/${file.name}`)
+                      .put(file, {});
+                    uploadTask.on(
+                      "state_changed",
+                      function (snapshot) {
+                        var progress =
+                          (snapshot.bytesTransferred / snapshot.totalBytes) *
+                          100;
+                        console.log("Upload is " + progress + "% done");
+                        switch (snapshot.state) {
+                          case firebase.storage.TaskState.PAUSED: // or 'paused'
+                            console.log("Upload is paused");
+                            break;
+                          case firebase.storage.TaskState.RUNNING: // or 'running'
+                            console.log("Upload is running");
+                            break;
+                        }
+                      },
+                      function (error) {
+                        reject(error);
+                      },
+                      function () {
+                        uploadTask.snapshot.ref
+                          .getDownloadURL()
+                          .then(function (url) {
+                            resolve({
+                              success: true,
+                              file: {
+                                url,
+                              },
+                            });
                           });
-                        });
-                    }
-                  );
-                });
-              },
-
-              /**
-               * Send URL-string to the server. Backend should load image by this URL and return an uploaded image data
-               * @param {string} url - pasted image URL
-               * @return {Promise.<{success, file: {url}}>}
-               */
-              uploadByUrl: async (url) => ({
-                success: true,
-                file: {
-                  url,
+                      }
+                    );
+                  });
                 },
-              }),
+
+                /**
+                 * Send URL-string to the server. Backend should load image by this URL and return an uploaded image data
+                 * @param {string} url - pasted image URL
+                 * @return {Promise.<{success, file: {url}}>}
+                 */
+                uploadByUrl: async (url) => ({
+                  success: true,
+                  file: {
+                    url,
+                  },
+                }),
+              },
             },
           },
+          list: {
+            class: List,
+            inlineToolbar: true,
+          },
+          table: {
+            class: Table,
+          },
+          page: Page,
+          splitPane: {
+            class: SplitPane,
+            inlineToolbar: true,
+          },
+          raw: Raw,
+          editorJSStyle: EditorJSStyle,
+          markdownParser: MDParser,
+          markdownImporter: MDImporter,
+          embed: {
+            class: Embed,
+          },
         },
-        list: {
-          class: List,
-          inlineToolbar: true,
-        },
-        table: {
-          class: Table,
-        },
-        page: Page,
-        splitPane: {
-          class: SplitPane,
-          inlineToolbar: true,
-        },
-        raw: Raw,
-        editorJSStyle: EditorJSStyle,
-        markdownParser: MDParser,
-        markdownImporter: MDImporter,
-        embed: {
-          class: Embed,
-        },
-      }, ...this.tools},
+        ...this.tools,
+      },
     });
   }
 
