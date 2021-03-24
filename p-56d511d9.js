c0f4a08296d0a941e1c53658ded4fc4d26f40e76
popcorn245 <popcorn245@users.noreply.github.com>
2021-03-24 18:23:10 +0000

    Deploying to gh-pages from master @ 80bc77f8d022a416135b0a17978100e9df7c6dee 🚀

--- p-56d511d9.js
+++
@@ -1,1 +0,0 @@
-import{d as o,w as t}from"./p-b19fa89e.js";import{c as s}from"./p-cbae79a1.js";const n=()=>{const n=window;n.addEventListener("statusTap",(()=>{o((()=>{const o=document.elementFromPoint(n.innerWidth/2,n.innerHeight/2);if(!o)return;const a=o.closest("ion-content");a&&new Promise((o=>s(a,o))).then((()=>{t((()=>a.scrollToTop(300)))}))}))}))};export{n as startStatusTap}
